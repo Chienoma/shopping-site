7e1e0a3cb5a8eacd64820142db0fcaf5f826f859
EC2 Default User <ec2-user@ip-172-31-67-253.ec2.internal>
2018-01-31 16:52:23 +0000

    shopping-site

---
+++ js/shopping-carousel.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel(
+        {
+            loop: true,
+            nav: false,
+            dots: true,
+            items: 1,
+            autoplay: true
+        }
+    );
+});
+
+// ハンバーガーメニュー
+$(document).ready(function(){
+  $("#sp_menu_btn").click(function () {
+    $(".toggle").slideToggle();
+  });
+});
+
+// スライドショー
+$(window).load(function(){
+  $("#list_img img").click(function(){
+    var img_src = $(this).attr("src");
+    $("#display_img img").attr("src", img_src);
+  });
+});
